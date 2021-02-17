4bb2da5563451aa1f142eb07048b1792bdffb66c
olga <itsglimerry@gmail.com>
2021-02-17 13:23:46 +0000

    Added web_helpers.rb to make feature tests more concise.

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,9 +4,7 @@ require 'spec_helper'
 feature 'Filling out the form' do
   scenario 'Can fill form out and posts valid value' do
     visit('/')
-    fill_in('player_1_name', with: 'John')
-    fill_in('player_2_name', with: 'John')
-    click_on('Submit')
-    expect(page).to have_content 'John vs. John'
+    sign_in_and_play
+    expect(page).to have_content 'John :60HP'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
-
+require './features/web_helpers'
 require 'rspec'
 
 # tell Capybara about our app class
