33c661cffd4c29e02254ad72ad881176682ce0a4
olga <itsglimerry@gmail.com>
2021-02-17 12:25:14 +0000

    Changed form to take two inputs. Updated tests and app.rb

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @name = params[:name]
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
     erb :play
   end
 
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,8 +4,9 @@ require 'spec_helper'
 feature 'Filling out the form' do
   scenario 'Can fill form out and posts valid value' do
     visit('/')
-    fill_in('name', with: 'John')
+    fill_in('player_1_name', with: 'John')
+    fill_in('player_2_name', with: 'John')
     click_on('Submit')
-    expect(page).to have_content 'John'
+    expect(page).to have_content 'John vs. John'
   end
 end
